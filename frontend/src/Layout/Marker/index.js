4a64fca9e7ea2a1a54d36a3f9e80218628aed3e4
Cules1017 <19521898@gmail.com>
2022-11-04 10:21:48 +0700

    add map

---
+++ frontend/src/Layout/Marker/index.js
@@ -0,0 +1,10 @@
+import { GoogleMap, Marker } from '@react-google-maps/api';
+function Markertic(center) {
+    return (
+        <div>
+            <Marker></Marker>
+        </div>
+    );
+}
+
+export default Markertic;
