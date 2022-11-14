427d642ca5b6b03a50c5c354a22f986553be4b99
Cules1017 <19521898@gmail.com>
2022-11-14 20:54:41 +0700

    add admin page fe

---
+++ frontend/src/Layout/Disaster/index.js
@@ -0,0 +1,30 @@
+import classNames from 'classnames/bind';
+import Button from '~/components/Button';
+import Input from '~/components/Input';
+import styles from './Info.module.scss';
+const cx = classNames.bind(styles);
+
+function DisasterList(props) {
+    return (
+        <div className={cx('wapper')}>
+            <h2 className={cx('title')}>Danh sách thiên tai</h2>
+            <div className={cx('colum2')}>
+                <div className={cx('insertcolumn')}></div>
+                <Button success small href="https://hndev.tech" onClick={() => alert('click đã đã')}>
+                    Thêm
+                </Button>
+            </div>
+            <Input name="name" title="Name" placeholder="Nguyễn Văn A"></Input>
+            <Input name="email" title="Email" placeholder="Nguyễn Văn A"></Input>
+            <Input small title="Password" type="password" placeholder="Passs1xy#"></Input>
+            <div className={cx('colum2')}>
+                <div className={cx('insertcolumn')}></div>
+                <Button primary medium href="https://hndev.tech" onClick={() => alert('click đã đã')}>
+                    Thêm QTV
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default DisasterList;
