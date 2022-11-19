138c6a071f7e692999da9eec9c7ed0fc933171ed
Cules1017 <19521898@gmail.com>
2022-11-19 16:13:07 +0700

    api backend fontend connect

--- frontend/src/pages/Register/validation.js
+++ frontend/src/pages/Register/validation.js
@@ -1,30 +1,28 @@
 const validation = (values) => {
+    let errors = {};
 
-    let errors  = {};
-
-    if(!values.fullname){
-        errors.fullname="required"
+    if (!values.name) {
+        errors.name = 'Trường này bắt buộc phải nhập';
     }
 
-    if(!values.email){
-        errors.fullname="required"
-    } else if(!/\S+@\S+\.\S+/.test(values.email)){
-        errors.email="invalid"
+    if (!values.email) {
+        errors.fullname = 'Trường này bắt buộc phải nhập';
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+        errors.email = 'Vui lòng nhập vào email đúng';
     }
 
-    if(!values.password){
-        errors.fullname="required"
-    } else if(values.password.length < 5){
-        errors.password=">5 character"
+    if (!values.password) {
+        errors.fullname = 'Trường này bắt buộc phải nhập';
+    } else if (values.password.length < 5) {
+        errors.password = 'Trường này phải >5 kí tự';
     }
 
-    if(values.password === values.repassword){
-        errors.repassword="required"
+    if (values.password === values.repassword) {
     } else {
-        errors.repassword="wrong"
+        errors.repassword = 'Mật khẩu trường này phải giống mật khẩu phía trên';
     }
 
     return errors;
-}
+};
 
 export default validation;
